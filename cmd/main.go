f495e23251c6b301d8003c6ea0e934c4a8862065
Agniva De Sarker <agnivade@yahoo.co.in>
2016-09-18 22:39:11 +0530

    stable version

--- cmd/main.go
+++ cmd/main.go
@@ -12,3 +12,14 @@ func main() {
 	c.Start()
 	defer c.CleanUp()
 }
+
+// take care to see that when the signal handler does not work,
+// the go-routine is ended.
+
+// problem - done is not getting called on interrupt
+
+// problem - signal handler is not getting called on clean end
+
+// TODO: if exit with log file exists, do not rollover that file
+
+// TODO: handle the return errors on all cases
--- funnel.go
+++ funnel.go
@@ -17,129 +17,171 @@ type Consumer struct {
 	currFile *os.File
 	writer   *bufio.Writer
 	feed     chan string
+
+	done         chan struct{}
+	rolloverChan chan struct{}
+	signal_chan  chan os.Signal
+	numLines     int
 }
 
 func (c *Consumer) Start() {
+	c.setupSignalHandling()
+	c.done = make(chan struct{})
+	c.rolloverChan = make(chan struct{})
+
 	// make the dir along with parents
-	err := os.MkdirAll(c.DirName, 0775)
-	if err != nil {
+	if err := os.MkdirAll(c.DirName, 0775); err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	c.newFile()
-
-	c.setupSignalHandling()
+	if err := c.newFile(); err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	c.feed = make(chan string)
 	go c.startFeed()
 
 	scanner := bufio.NewScanner(os.Stdin)
-	lines := 0
+	c.numLines = 0
 	for scanner.Scan() {
-		lines++
+		c.numLines++
 		if c.rollOverCondition() {
-			c.rollOver()
+			c.rolloverChan <- struct{}{}
 		}
 		c.feed <- scanner.Text()
 	}
 	if err := scanner.Err(); err != nil {
-		fmt.Println(err)
+		fmt.Println("scanner stopped- ", err)
 	}
-	// close channel
-	close(c.feed)
+	// work is done, signalling done channel
+	c.done <- struct{}{}
+	// quitting from signal handler
+	close(c.signal_chan)
 }
 
 func (c *Consumer) CleanUp() {
 	// flush writer
-	c.writer.Flush()
+	if c.writer != nil {
+		c.writer.Flush()
+	}
 
 	// close file handle
-	err := c.currFile.Sync()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	err = c.currFile.Close()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	// Rename the currfile to a rolled up one
-	c.rename()
-}
-
-func (c *Consumer) newFile() {
-	f, err := os.OpenFile(path.Join(c.DirName, "out.log"),
-		os.O_CREATE|os.O_WRONLY|os.O_TRUNC|os.O_APPEND|os.O_EXCL,
-		0644)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	c.currFile = f
-	c.writer = bufio.NewWriter(c.currFile)
-}
-
-func (c *Consumer) rollOverCondition() bool {
-	return false
-}
-
-func (c *Consumer) rollOver() {
-	// flush writer
-	c.writer.Flush()
-
-	// close file handle
-	err := c.currFile.Sync()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	err = c.currFile.Close()
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	c.rename()
-
-	c.newFile()
-}
-
-func (c *Consumer) rename() {
-	t := time.Now()
-	err := os.Rename(
-		path.Join(c.DirName, "out.log"),
-		path.Join(c.DirName, t.Format("15_04_05-2006_01_02")+".log"),
-	)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-}
-
-func (c *Consumer) startFeed() {
-	for line := range c.feed {
-		_, err := fmt.Fprintln(c.writer, line)
+	if c.currFile != nil {
+		err := c.currFile.Sync()
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		c.writer.Flush()
+		err = c.currFile.Close()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	// Rename the currfile to a rolled up one
+	if err := c.rename(); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func (c *Consumer) newFile() error {
+	f, err := os.OpenFile(path.Join(c.DirName, "out.log"),
+		os.O_CREATE|os.O_WRONLY|os.O_TRUNC|os.O_APPEND|os.O_EXCL,
+		0644)
+	if err != nil {
+		return err
+	}
+	c.currFile = f
+	c.writer = bufio.NewWriter(c.currFile)
+	return nil
+}
+
+func (c *Consumer) rollOverCondition() bool {
+	return c.numLines%40 == 0
+}
+
+func (c *Consumer) rollOver() error {
+	// flush writer
+	err := c.writer.Flush()
+	if err != nil {
+		return err
+	}
+
+	// close file handle
+	if err := c.currFile.Sync(); err != nil {
+		return err
+	}
+
+	if err := c.currFile.Close(); err != nil {
+		return err
+	}
+
+	if err := c.rename(); err != nil {
+		return err
+	}
+
+	if err := c.newFile(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Consumer) rename() error {
+	t := time.Now()
+	err := os.Rename(
+		path.Join(c.DirName, "out.log"),
+		path.Join(c.DirName, t.Format("15_04_05.000-2006_01_02")+".log"),
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Consumer) startFeed() {
+	// Will flush the writer every 5 sec
+	ticker := time.NewTicker(5 * time.Second)
+	for {
+		select {
+		case line := <-c.feed:
+			_, err := fmt.Fprintln(c.writer, line)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		case <-c.rolloverChan:
+			if err := c.rollOver(); err != nil {
+				fmt.Println(err)
+				return
+			}
+		case <-c.done:
+			ticker.Stop()
+			if err := c.writer.Flush(); err != nil {
+				fmt.Println(err)
+			}
+			return
+		case <-ticker.C:
+			if err := c.writer.Flush(); err != nil {
+				fmt.Println(err)
+			}
+		}
 	}
 }
 
 func (c *Consumer) setupSignalHandling() {
-	signal_chan := make(chan os.Signal, 1)
-	signal.Notify(signal_chan,
+	c.signal_chan = make(chan os.Signal, 1)
+	signal.Notify(c.signal_chan,
 		os.Interrupt, syscall.SIGPIPE)
 
 	// Block until a signal is received.
-	go func(signal_chan chan os.Signal) {
-		<-signal_chan
-		c.CleanUp()
-		os.Exit(1)
-	}(signal_chan)
+	// Or EOF happens
+	go func() {
+		for _ = range c.signal_chan {
+			fmt.Println("Caught signal")
+		}
+	}()
 }
